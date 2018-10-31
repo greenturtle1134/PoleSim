5f69cbd3a275fb18635899a7316622000a67265b
danie <danie@Inspiron7000>
2018-10-31 11:47:42 -0400

    Set it to create three windows with different exponents.

--- src/PoleApp.java
+++ src/PoleApp.java
@@ -3,16 +3,20 @@ import javax.swing.JFrame;
 public class PoleApp {
 
 	public static void main(String[] args) {
-		
-		PoleSim sim = new PoleSim(1, 1200, 1200, 400, 0.2, 600, 300, 8, 0);
+		startSim(new PoleSim(1, 1200, 1200, 100, 0.25, 600, 400, 12, 0));
+		startSim(new PoleSim(2, 1200, 1200, 100000, 0.25, 600, 400, 15, 0));
+		startSim(new PoleSim(3, 1200, 1200, 10000000, 0.25, 600, 400, 15, 0));
+
+	}
+	
+	public static void startSim(PoleSim sim) {
 		JFrame frame = new JFrame("POLE");
-		frame.setSize(1200, 1200);
+		frame.setSize(400, 400);
 		frame.setContentPane(sim);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		Thread thread = new Thread(sim);
 		thread.start();
-
 	}
 
 }
